a7ff0487ea0db0f7ecd6e8a1c1175fc7488bcf79
Zoran Repic <zoran.repic@genairate.io>
2025-10-07 21:57:15 +0200

    feat: pair board implementation with test

--- lib/src/main/kotlin/org/jetbrains/kotlinx/tictactoe/model/Board.kt
+++ lib/src/main/kotlin/org/jetbrains/kotlinx/tictactoe/model/Board.kt
@@ -1,4 +1,10 @@
 package org.jetbrains.kotlinx.tictactoe.model
 
 class Board {
+    private val cells = MutableList(9) { Mark.EMPTY }
+
+    fun getCells(): List<Mark> = cells.toList()
+
+    fun availableMoves(): List<Int> =
+        cells.indices.filter { cells[it] == Mark.EMPTY }
 }
--- lib/src/test/kotlin/org/jetbrains/kotlinx/tictactoe/BoardTest.kt
+++ lib/src/test/kotlin/org/jetbrains/kotlinx/tictactoe/BoardTest.kt
@@ -1,8 +1,10 @@
 package org.jetbrains.kotlinx.tictactoe
 
 import junit.framework.TestCase.assertTrue
+import org.jetbrains.kotlinx.tictactoe.model.Board
 import org.jetbrains.kotlinx.tictactoe.model.Mark
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 class BoardTest{
@@ -10,6 +12,14 @@ class BoardTest{
     fun `Mark should have X O and EMPTY`() {
         val values = Mark.values().toList()
         assertTrue(values.containsAll(listOf(Mark.X, Mark.O, Mark.EMPTY)))
+    }
+
+
+    @Test
+    fun `board starts empty`() {
+        val board = Board()
+        assertEquals(List(9) { Mark.EMPTY }, board.getCells())
+        assertEquals((0..8).toList(), board.availableMoves())
     }
 
 }
